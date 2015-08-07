1a1ec7eaca32a50120cdf948285ee4769d006fd7
Tamsyn Murphy <tamsyn.murphy@gmail.com>
2015-08-07 12:18:26 +0100

    New files

---
+++ FileException1.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class FileException1
+{
+	public static void main(String[] args)
+	{
+		openFile("C:\test.txt");
+	}
+	
+	public static void openFile(String name)
+	{
+		FileInputStream f = new FileInputStream(name);
+	}
+}
---
+++ HelloApp.java
@@ -0,0 +1,10 @@
+public class HelloApp
+{
+	static String helloMessage;
+	
+	public static void main(String[] args)
+	{
+		helloMessage = "Hello, World!";
+		System.out.println(helloMessage);
+	}
+}
